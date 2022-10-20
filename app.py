c9bed8255aba74a8c5a6cae26431bc9d89b60980
atgane <hyper2012@naver.com>
2022-10-20 11:37:51 +0900

    Init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    a = 0
+    for i in range(1000):
+        a += i
+    return f'{a}'
+
+if __name__ == '__main__':
+    app.run()
